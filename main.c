f9d486f6547a1e386efea8d9d6f32817c2a47332
Muhammad Ali Shahmeer Khan <alishahmeerkhan05@gmail.com>
2026-05-10 15:57:24 +0500

    feat: completed child tracking, logging and added time calculations as well

--- main.c
+++ main.c
@@ -15,12 +15,13 @@
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		printf("%s-- Error --%s\n", COLOR_BOLD_RED, COLOR_RESET);
-		printf("%sCommand Line Format: 		./main -p pid		[Track External Process]%s\n", COLOR_GREY, COLOR_RESET);
-		printf("%sCommand Line Format:		./main <command>	[Track Child Process]%s\n", COLOR_GREY, COLOR_RESET);
+		printf("%sCommand Line Format: 		sudo ./main -p pid		[Track External Process]%s\n", COLOR_GREY, COLOR_RESET);
+		printf("%sCommand Line Format:		sudo ./main <command>	[Track Child Process]%s\n", COLOR_GREY, COLOR_RESET);
 		return 1;
 	}
 
 	pid_t target_pid;
+	time_t start_time = time(NULL);
 
 	if (strncmp(argv[1], "-p", 2) == 0) {  /* Track External Process  */
 		pid_t pid;
@@ -57,6 +58,9 @@ int main(int argc, char *argv[]) {
 			waitpid(target_pid, &status, 0);
 
 			if (WIFEXITED(status)) {
+				time_t end_time = time(NULL);
+				double elapsed_time = difftime(end_time, start_time);
+				printf("[INFO] Total Tracking Time: %s%.2f seconds%s\n", COLOR_BOLD_GREEN, elapsed_time, COLOR_RESET);
 				printf("\n\n");
 				printf("[INFO] External Process with PID %s%d%s exited with status %s%d%s\n", COLOR_CYAN, target_pid, COLOR_RESET, COLOR_YELLOW, WEXITSTATUS(status), COLOR_RESET);
 				break;
@@ -73,6 +77,9 @@ int main(int argc, char *argv[]) {
 					ptrace(PTRACE_GETREGS, target_pid, NULL, &regs);
 					unsigned long syscall_id = regs.orig_rax;
 
+					time_t end_time = time(NULL);;
+					double elapsed_time = difftime(end_time, start_time);
+					printf("%f", elapsed_time);
 					printf("[INFO] Process %s%d%s called System Call: %s%lu%s\n", COLOR_CYAN, target_pid, COLOR_RESET, COLOR_BOLD_GREEN, syscall_id, COLOR_RESET);
 					// log_syscall_entry(target_pid, syscall_id);
 
@@ -124,6 +131,30 @@ int main(int argc, char *argv[]) {
 					break;
 				}
 				else if (WIFSTOPPED(status)) {
+					static int isEntry = 1;
+
+					if (isEntry) {
+						struct user_regs_struct regs;
+						ptrace(PTRACE_GETREGS, target_pid, NULL, &regs);
+						unsigned long syscall_id = regs.orig_rax;
+
+						time_t end_time = time(NULL);
+						double elapsed_time = difftime(end_time, start_time);
+						printf("%f", elapsed_time);
+						printf("[INFO] Process %s%d%s called System Call: %s%lu%s\n", COLOR_CYAN, target_pid, COLOR_RESET, COLOR_BOLD_GREEN, syscall_id, COLOR_RESET);
+						// log_syscall_entry(target_pid, syscall_id);
+
+						FILE *log_file = fopen("syscall_log.log", "a");
+    					fprintf(log_file, "[PID %d] Entered syscall: %lu\n", target_pid, syscall_id);
+    					fclose(log_file);
+					
+						isEntry = 0;
+						ptrace(PTRACE_SYSCALL, target_pid, NULL, NULL);
+					}
+					else {
+						isEntry = 1;
+					}
+
 					ptrace(PTRACE_SYSCALL, target_pid, NULL, NULL);
 					continue;
 				}
